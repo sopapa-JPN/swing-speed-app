3a74ca941fd67a77f0b0f97d592d021aa5e448c8
sopapa-JPN <tokukurasatoshi@gmail.com>
2026-07-21 15:36:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const C='swing-speed-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',()=>{});
